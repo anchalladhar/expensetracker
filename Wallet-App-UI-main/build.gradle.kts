6bd6209480cfa272f4d5a4e25240d7abbb06c57e
anchalladhar <133622298+anchalladhar@users.noreply.github.com>
2025-02-22 00:10:51 -0500

    Update build.gradle.kts

--- Wallet-App-UI-main/build.gradle.kts
+++ Wallet-App-UI-main/build.gradle.kts
@@ -1,26 +1,15 @@
 plugins {
-    kotlin("multiplatform") version "1.8.0" // Use the appropriate version
+    kotlin("android") version "1.8.0" // Use the appropriate version
 }
 
 kotlin {
     android()
-    ios() // You can add other targets like iosX64, iosArm64, etc.
 
     sourceSets {
-        val commonMain by getting {
+        val main by getting {
             dependencies {
                 implementation(kotlin("stdlib"))
-                // Add common dependencies here
-            }
-        }
-        val androidMain by getting {
-            dependencies {
-                // Android-specific dependencies
-            }
-        }
-        val iosMain by getting {
-            dependencies {
-                // iOS-specific dependencies
+                // Add Android-specific dependencies here
             }
         }
     }
